6360af50a6977f64de122d743c5e304670e78caa
tangyubin <tangyubin@tal.com>
2024-12-06 18:35:29 +0800

    feat: init

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func main() {
+
+	app := fiber.New()
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":9999"))
+
+}
