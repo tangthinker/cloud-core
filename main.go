0df8862b82ee3052e2b3390bb5b4f9a22b083f66
tangyubin <tangthinker@yeah.net>
2025-01-02 15:01:06 +0800

    feat: add logging

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/tangthinker/cloud-core/api/storage"
 	"log"
+	"os"
 )
 
 func main() {
@@ -12,6 +14,18 @@ func main() {
 	app := fiber.New(fiber.Config{
 		BodyLimit: 800 * 1024 * 1024, // 800MB
 	})
+
+	loggingFile, err := os.OpenFile("requests.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.Use(logger.New(logger.Config{
+		Format:     "${time} ${method} ${path} - ${ip} - ${status} - ${latency}\n body: ${body}\n",
+		TimeFormat: "2006-01-02 15:04:05",
+		TimeZone:   "Local",
+		Output:     loggingFile,
+	}))
 
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Hello, World!")
