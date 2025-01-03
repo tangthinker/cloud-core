af7d9f78eb60d5af294213a5a2eab7085b7f6f7d
tangyubin <tangthinker@yeah.net>
2025-01-03 18:18:29 +0800

    feat: online

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 		BodyLimit: 800 * 1024 * 1024, // 800MB
 	})
 
-	loggingFile, err := os.OpenFile("requests.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	loggingFile, err := os.OpenFile("/home/tangthinker/code/go-projects/cloud-core/requests.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,7 +34,7 @@ func main() {
 
 	apiGroup := app.Group("/api/v1/storage/", middleware.TokenValid)
 
-	api := storage.NewApi("/Users/tal/Downloads")
+	api := storage.NewApi("/home/tangthinker/backups")
 
 	apiGroup.Post("/ls", api.LS)
 	apiGroup.Get("/get", api.Get)
