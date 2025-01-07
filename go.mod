553cad88e37eac4803034b2143b6504bce257033
tangyubin <tangthinker@yeah.net>
2025-01-07 15:54:12 +0800

    feat: add ffmpeg operation

--- go.mod
+++ go.mod
@@ -11,10 +11,12 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/aws/aws-sdk-go v1.38.20 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -23,6 +25,11 @@ require (
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/tangthinker/jwt-model v1.0.0 // indirect
 	github.com/tangthinker/user-center v1.0.4 // indirect
+	github.com/tidwall/gjson v1.18.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/u2takey/ffmpeg-go v0.5.0 // indirect
+	github.com/u2takey/go-utils v0.3.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.55.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
---
+++ pkg/video_trans/ffmpeg.go
@@ -0,0 +1,88 @@
+package video_trans
+
+import (
+	"fmt"
+	"github.com/tidwall/gjson"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+	"math/rand"
+	"net"
+	"os"
+	"path"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func TransWithProgress(inFileName, outFileName string, onProgress func(progress string), onErr func(err error)) {
+	a, err := ffmpeg.Probe(inFileName)
+	if err != nil {
+		onErr(err)
+	}
+	totalDuration := gjson.Get(a, "format.duration").Float()
+
+	err = ffmpeg.Input(inFileName).
+		Output(outFileName,
+			ffmpeg.KwArgs{
+				"profile:v":     "baseline", // 设置视频配置文件为baseline
+				"level":         "3.0",      // 设置视频编码级别
+				"start_number":  "0",        // 设置分片开始的序号
+				"hls_time":      "10",       // 每个HLS分片的时长为10秒
+				"hls_list_size": "0",        // 不限制播放列表中的M3U8片段数量
+				"f":             "hls",      // 设置输出格式为HLS
+			},
+		).
+		GlobalArgs("-progress", "unix://"+TempSock(totalDuration, onProgress, onErr)).
+		OverWriteOutput().
+		Run()
+	if err != nil {
+		onErr(err)
+	}
+}
+
+func TempSock(totalDuration float64, onProgress func(progress string), onErr func(err error)) string {
+	// serve
+
+	sockFileName := path.Join(os.TempDir(), fmt.Sprintf("%d_sock", rand.Int()))
+	l, err := net.Listen("unix", sockFileName)
+	if err != nil {
+		onErr(err)
+	}
+
+	go func() {
+		re := regexp.MustCompile(`out_time_ms=(\d+)`)
+		fd, err := l.Accept()
+		if err != nil {
+			onErr(err)
+		}
+		buf := make([]byte, 16)
+		data := ""
+		progress := ""
+		for {
+			_, err := fd.Read(buf)
+			if err != nil {
+				return
+			}
+			data += string(buf)
+			a := re.FindAllStringSubmatch(data, -1)
+			cp := ""
+			if len(a) > 0 && len(a[len(a)-1]) > 0 {
+				c, _ := strconv.Atoi(a[len(a)-1][len(a[len(a)-1])-1])
+				cp = fmt.Sprintf("%.2f", float64(c)/totalDuration/1000000*100)
+				onProgress(cp + "%")
+			}
+			if strings.Contains(data, "progress=end") {
+				cp = "done"
+				onProgress("100.00%")
+			}
+			if cp == "" {
+				cp = ".0"
+				onProgress("0.00%")
+			}
+			if cp != progress {
+				progress = cp
+			}
+		}
+	}()
+
+	return sockFileName
+}
