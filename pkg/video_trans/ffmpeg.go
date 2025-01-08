c48a506040d60dcbc9288a8ab4f0a6d7b509e766
tangyubin <tangthinker@yeah.net>
2025-01-08 10:26:58 +0800

    feat: optimizer

--- pkg/video_trans/ffmpeg.go
+++ pkg/video_trans/ffmpeg.go
@@ -67,19 +67,17 @@ func TempSock(totalDuration float64, onProgress func(progress string), onErr fun
 			cp := ""
 			if len(a) > 0 && len(a[len(a)-1]) > 0 {
 				c, _ := strconv.Atoi(a[len(a)-1][len(a[len(a)-1])-1])
-				cp = fmt.Sprintf("%.2f", float64(c)/totalDuration/1000000*100)
+				cp = fmt.Sprintf("%.2f%", float64(c)/totalDuration/1000000*100)
 			}
 			if strings.Contains(data, "progress=end") {
 				cp = "100.00%"
-				onProgress("100.00%")
 			}
 			if cp == "" {
 				cp = "0.00%"
-				onProgress("0.00%")
 			}
 			if cp != progress {
 				progress = cp
-				onProgress(cp + "%")
+				onProgress(cp)
 			}
 		}
 	}()
